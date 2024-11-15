d441e6aad42d82fcca62249f68480721d3b80831
Luis <luismanuel770@ciencias.unam.mx>
2024-11-15 14:32:18 -0600

    Se agregó el primer archivo

---
+++ hola_mundo.cbl
@@ -0,0 +1,23 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Divisiones.
+
+       AUTHOR. KIFF.
+       INSTALLATION.
+       DATE-WRITTEN.15/09/24
+       DATE-COMPILED.16/09/24
+       REMARKS. Programa creado para ver las divisiones de COBOL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hola mundillo"
+            STOP RUN.
+       END PROGRAM Divisiones.
